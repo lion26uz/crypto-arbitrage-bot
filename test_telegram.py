a43dadc4a7419d3ee3f16b504bd69edbdb33cc15
lion26uz <plus48570735434@gmail.com>
2025-07-24 11:57:08 +0200

    Update test_telegram.py

--- test_telegram.py
+++ test_telegram.py
@@ -1,16 +1,19 @@
 import requests
 
-token = "6103456789:AAExampleTokenYourBot123456"  # ← твой токен
-chat_id = "123456789"  # ← твой chat_id
+token = "6103456789:AAExampleTokenYourBot123456"  # сюда твой токен
+chat_id = "123456789"  # сюда твой chat_id
 
 message = "✅ Telegram уведомление работает!"
 
 url = f"https://api.telegram.org/bot{token}/sendMessage"
-data = {"chat_id": chat_id, "text": message}
+data = {
+    "chat_id": chat_id,
+    "text": message
+}
 
 response = requests.post(url, data=data)
 
 if response.status_code == 200:
-    print("Сообщение отправлено.")
+    print("Сообщение отправлено в Telegram!")
 else:
     print("Ошибка:", response.text)
