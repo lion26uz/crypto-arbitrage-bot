29870573cc4904e677e1e0dc0dd5d4451553da43
lion26uz <plus48570735434@gmail.com>
2025-07-24 11:48:18 +0200

    Add files via upload

---
+++ test_telegram.py
@@ -0,0 +1,5 @@
+
+from telegram import Bot
+
+bot = Bot(token="bot_saryuch")  # Замените на ваш токен
+bot.send_message(chat_id=123456789, text="✅ Telegram уведомление работает!")  # Замените на ваш chat_id
